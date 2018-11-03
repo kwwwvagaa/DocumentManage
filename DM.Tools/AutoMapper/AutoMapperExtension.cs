c26bfba20a69a024956e514b72886d909a1ab861
kwwwvagaa <623128629@qq.com>
2018-11-03 11:34:49 +0800

    Create AutoMapperExtension.cs

---
+++ DM.Tools/AutoMapper/AutoMapperExtension.cs
@@ -0,0 +1,33 @@
+/// <summary>
+    /// AutoMapper扩展方法
+    /// </summary>
+    public static class AutoMapperExtension
+    {
+        /// <summary>
+        /// 集合对集合
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static List<TResult> MapTo<TResult>(this IEnumerable self)
+        {
+            if (self == null)
+                throw new ArgumentNullException();
+            Mapper.CreateMap(self.GetType(), typeof(TResult));
+            return (List<TResult>)Mapper.Map(self, self.GetType(), typeof(List<TResult>));
+        }
+        /// <summary>
+        /// 对象对对象
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static TResult MapTo<TResult>(this object self)
+        {
+            if (self == null)
+                throw new ArgumentNullException();
+            Mapper.CreateMap(self.GetType(), typeof(TResult));
+            return (TResult)Mapper.Map(self, self.GetType(), typeof(TResult));
+        }
+
+    }
